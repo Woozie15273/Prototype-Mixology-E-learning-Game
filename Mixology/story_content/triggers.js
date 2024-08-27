86c7fb5377b0d0176cd29bdd9df2e992985e3869
Woozie15273 <56500768+Woozie15273@users.noreply.github.com>
2024-08-27 10:51:20 -0700

    Add files via upload

---
+++ Mixology/story_content/triggers.js
@@ -0,0 +1,57 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5bDJ7juh7gI":
+        Script1();
+        break;
+      case "6FSJXyIfbHC":
+        Script2();
+        break;
+      case "6LJb1w2hDtg":
+        Script3();
+        break;
+      case "6hSgT7J95l7":
+        Script4();
+        break;
+      case "6ctokvJiB8k":
+        Script5();
+        break;
+      case "6lu14DpWmtP":
+        Script6();
+        break;
+      case "5q4yUNbIGMO":
+        Script7();
+        break;
+      case "63qrIkHZlkV":
+        Script8();
+        break;
+      case "5f43i0vjNsd":
+        Script9();
+        break;
+      case "6BOaukFjrO1":
+        Script10();
+        break;
+      case "6TUDNEk5iWq":
+        Script11();
+        break;
+      case "65rbiWutlGC":
+        Script12();
+        break;
+      case "6YB9KQlXZzp":
+        Script13();
+        break;
+      case "5ZtbqgJhnsb":
+        Script14();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
